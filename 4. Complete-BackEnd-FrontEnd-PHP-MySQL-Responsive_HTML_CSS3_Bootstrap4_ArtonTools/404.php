0e7fadaf6c0c035d6dd1d65d597714fb3c75cfe1
Brian Sam Thomas <BRAiNCHiLD95@github.com>
2018-11-29 12:44:50 +0530

    Create 404.php
    404 page for the front-end

---
+++ 4. Complete-BackEnd-FrontEnd-PHP-MySQL-Responsive_HTML_CSS3_Bootstrap4_ArtonTools/404.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AC | HomePage</title>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <style>
+        body {
+            height: 100% !important;
+            background-color: #CC9966;
+        }
+        footer {
+            position: absolute;
+            bottom: 0;
+        }
+    </style>
+</head>
+
+<body>
+    <?php include('header.php'); ?>
+        <div class="landing text-center pt-5">
+            <div class="landing-content">
+                <div class="container-fluid mt-5 pt-5 text-center">
+                    <h1 class="display-1 mt-5 pt-5" style="color:#CC9966;">404</h1>
+                    <p class="text-white lead">Page not found. You can
+                    <a style="color: #CC9966; text-decoration:none;" href="javascript:history.back()">go back</a>
+                    to the previous page, or
+                    <a style="color: #CC9966; text-decoration:none;" href="index.php">return home</a>.</p>
+                </div>
+            </div>
+        </div>
+
+    </section>
+    <?php include('footer.php'); ?>
+</body>
+</html>
